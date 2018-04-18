54d935666483be79b07c240d02ba36a2e62387c3
flpfar <flp.far@gmail.com>
2018-04-18 00:45:36 -0300

    main screen design idea

--- app/src/main/java/br/edu/ifspsaocarlos/sdm/fototagz/MainActivity.java
+++ app/src/main/java/br/edu/ifspsaocarlos/sdm/fototagz/MainActivity.java
@@ -1,13 +1,41 @@
 package br.edu.ifspsaocarlos.sdm.fototagz;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final CardView cvFototag = (CardView) findViewById(R.id.cv_fototag);
+        final CardView cvGaleria = (CardView) findViewById(R.id.cv_galeria);
+        final CardView cvCamera = (CardView) findViewById(R.id.cv_camera);
+
+        cvFototag.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
+        cvGaleria.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        cvCamera.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
