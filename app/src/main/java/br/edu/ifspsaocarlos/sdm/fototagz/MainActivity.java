48dd1d4abd6df3257c45383d6569daa6fc904ae0
flpfar <flp.far@gmail.com>
2018-04-24 22:28:40 -0300

    camera permission, getting image from camera (test)

--- app/src/main/java/br/edu/ifspsaocarlos/sdm/fototagz/MainActivity.java
+++ app/src/main/java/br/edu/ifspsaocarlos/sdm/fototagz/MainActivity.java
@@ -1,26 +1,39 @@
 package br.edu.ifspsaocarlos.sdm.fototagz;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private CardView cvFototag;
+    private CardView cvGaleria;
+    private CardView cvCamera;
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final CardView cvFototag = (CardView) findViewById(R.id.cv_fototag);
-        final CardView cvGaleria = (CardView) findViewById(R.id.cv_galeria);
-        final CardView cvCamera = (CardView) findViewById(R.id.cv_camera);
+        cvFototag = (CardView) findViewById(R.id.cv_fototag);
+        cvGaleria = (CardView) findViewById(R.id.cv_galeria);
+        cvCamera = (CardView) findViewById(R.id.cv_camera);
+        mImageView = (ImageView) findViewById(R.id.imageViewTeste);
+
 
         cvFototag.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View v) {
-                
+
             }
         });
 
@@ -34,8 +47,23 @@ public class MainActivity extends Activity {
         cvCamera.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View v) {
-
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
             }
         });
+
+
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+
 }
